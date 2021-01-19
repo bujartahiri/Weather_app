a99d5cef256269156ed87be0186471e6b990a4c2
Bujar Tahiri <bujaar.tahiri@gmail.com>
2021-01-19 04:55:03 +0100

    requests.js, get city by name api call

---
+++ public/requests.js
@@ -0,0 +1,13 @@
+const API_KEY = '8852a1f0ef415115afe8c4c8b0cd241a'
+
+const requestCity = async city => {
+  const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+  const query= `?q=${city}&appid=${API_KEY}`
+
+  const response = await fetch(baseURL + query)
+  
+  const data = await response.json()
+  return data
+}
+
+requestCity('Mitrovice')
