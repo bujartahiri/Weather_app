c81165c5ccb7c1c37eb68697ff55c73e4a858b86
Bujar Tahiri <bujaar.tahiri@gmail.com>
2021-01-21 23:34:40 +0100

    Submit form after clicking city suggestion

--- public/searchbox.js
+++ public/searchbox.js
@@ -1,7 +1,24 @@
 const input = document.querySelector('[data-city-cearch]')
 const searchBox = new google.maps.places.Autocomplete(input)
 
-searchBox.addListener('places_changed', () => {
-  const place = input.getPlace()
-  if (place == null) return
+searchBox.addListener("place_changed", () => {
+  const place = searchBox.getPlace()
+
+  if(place.length == 0) return
+
+ 
+   requestCity(place.name)
+    .then(data => {
+      updateWeatherApp(data)
+      searchForm.reset()
+    })
+    .catch(error => {
+      console.error(error)
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: "We couldn't find the city you searched",
+      })
+    })
+    
 })
